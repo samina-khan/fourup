a32b380caeb666a8557a99c994bb07f1c5d593e8
Samina Khan <skhan2@pdx.edu>
2015-07-03 22:10:32 -0700

    Added GlobalConstants interface for all classes to implement so as to not have 'magic numbers'.

---
+++ app/src/main/java/samina/fourup/GlobalConstants.java
@@ -0,0 +1,15 @@
+/*
+Copyright (c) 2015 Samina Khanam
+
+This source file is licensed under the "MIT license”.
+Please see the file COPYING in this distribution
+for license terms.
+*/
+
+package samina.fourup;
+
+/**
+ * Created by SAMINA on 7/3/15.
+ */
+public interface GlobalConstants {
+}
