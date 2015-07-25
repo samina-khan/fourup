d035194447624ecb6e09d68ba0baf94309710add
Samina Khan <skhan2@pdx.edu>
2015-07-25 11:26:09 -0700

    Fixed: Previously the clicked slot would change color. Now click fixed to make bottom-most available slot change color.

--- app/src/main/java/samina/fourup/GameBoard.java
+++ app/src/main/java/samina/fourup/GameBoard.java
@@ -22,14 +22,38 @@ public class GameBoard implements GlobalConstants{
            holes = new Hole[numrows][numcols];
     }
 
-    public void onclick(ImageView i){
-        i.setImageResource(colors[turn]);
-        toggleTurn();
-        for(Hole[] array : holes){
-            for(Hole h : array){
-                if(h.v == i) h.filled = turn;
+    //Similar to a mediator pattern, receives the click event from one hole
+    // and colors the first available hole in that column
+    public void onclick(ImageView imageview){
+        int theColumn = 0;
+        for(int i=0 ; i<standardNumRows ; i++){
+            for (int j=0; j<standardNumCols ; j++){
+                if((holes[i][j].v == imageview)){
+                    theColumn = j;
+                    break;
+                }
             }
         }
+        for (int i = standardNumRows - 1; i >= 0 ; i--){
+            System.out.println("[i][theColumn]: ["+i+"]["+theColumn+"]");
+            if(holes[i][theColumn].filled == GCBlack){
+                System.out.println("i:"+i);
+                holes[i][theColumn].filled = turn;
+                holes[i][theColumn].v.setImageResource(colors[turn]);
+                toggleTurn();
+                break;
+            }
+        }
+        /*
+        for(Hole[] array : holes){
+            for(Hole h : array){
+                if((h.v == i) && (h.filled == GCBlack)){
+                    h.filled = turn;
+                    i.setImageResource(colors[turn]);
+                    toggleTurn();
+                }
+            }
+        }*/
 
     }
 
